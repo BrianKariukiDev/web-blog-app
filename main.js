52ced63b8134ea1bdc170251d7834a181f4a7a69
Brian Kariuki <kariukibrian087@gmail.com>
2023-10-28 08:40:19 +0300

    Finish frontend

--- main.js
+++ main.js
@@ -17,3 +17,24 @@ const closeNav=()=>{
 }
 
 closeNavBtn.addEventListener('click',closeNav);
+
+
+
+const sidebar= document.querySelector('aside');
+const showSidebarBtn= document.querySelector('#show_sidebar-btn');
+const hideSidebarBtn= document.querySelector('#hide_sidebar-btn');
+
+const showSidebar=()=> {
+    sidebar.style.left = '0';
+    showSidebarBtn.style.display= 'none';
+    hideSidebarBtn.style.display='block';
+}
+
+const hideSidebar=()=> {
+    sidebar.style.left = '-100%';
+    showSidebarBtn.style.display='block';
+    hideSidebarBtn.style.display='none';
+}
+
+showSidebarBtn.addEventListener('click',showSidebar);
+hideSidebarBtn.addEventListener('click',hideSidebar);
