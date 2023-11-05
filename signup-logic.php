df98cb4103f5404976bde60a639846c2b25b88aa
Brian Kariuki <kariukibrian087@gmail.com>
2023-11-05 11:38:35 +0300

    some updates

--- signup-logic.php
+++ signup-logic.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
     require_once "config/database.php";
 
     if(isset($_POST['submit'])){
@@ -28,26 +29,56 @@
         }
         elseif(!$avatar['name']){
             $_SESSION['signup']= "Please add avatar";
-        }
-        else{
+        }else{
             if($createpassword !== $confirmpassword){
-                echo "Passwords do not match";
-            }
-            else{
+                $_SESSION['signup']="Passwords do not match";
+            }else{
                 $hashedpassword=password_hash($createpassword,PASSWORD_DEFAULT);
             }
 
             $usercheck= "SELECT * FROM users WHERE username='$username' OR email='$email'";
-            $usercheckresults=mysqli_query($connection,$usercheck);
+            $usercheckresults=mysqli_query($conn,$usercheck);
             if(mysqli_num_rows($usercheckresults)>0){
                 $_SESSION['signup']="Username or Email already exists";
-            }
-            else{
-                
+            }else{
+                $time=time();
+                $avatar_name= $time.$avatar['name'];
+                $avatar_temp_name=$avatar["tmp_name"];
+                $avatar_destination_path= 'images2/'. $avatar_name;
+
+                $allowed_files=['png','jpg','jpeg'];
+                $extension=explode('.',$avatar_name);
+                $extension=end($extension);
+                if(in_array($extension,$allowed_files)){
+                    if($avatar['size']<1100000){
+                        move_uploaded_file($avatar_temp_name,$avatar_destination_path);
+                    }else{
+                        $_SESSION['signup']= "File size should be less than 1mb";
+                    }
+                }else{
+                    $_SESSION['signup']="File should be in png,jpg or jpeg form"; 
+                }
             }
         }
-    }
-    else{
+        
+        // redirect to sign up page if error
+        if($_SESSION['signup']){
+            // session to pass back the data keyed in 
+            $_SESSION['signup-data']= $_POST;
+            header('location:'.ROOT_URL.'signup.php');
+            die();
+        }else{
+            // insert to databasse
+            $insert_user_query="INSERT INTO users (firstname,lastname,username,email,password,avatar,is_admin) VALUES('$firstname','$lastname','$username','$email','$hashedpassword','$avatar_name',0)";
+            mysqli_query($conn,$insert_user_query);
+
+            if(!mysqli_errno($conn)){
+                $_SESSION['signup-success']= "Registration successful.Log in";
+                header('location:'.ROOT_URL.'signin.php');
+                die();
+            }
+        }
+    }else{
         header('location:' .ROOT_URL.'signup.php');
         die();
     }
--- signup.php
+++ signup.php
@@ -1,5 +1,16 @@
 <?php
+session_start();
 require_once 'config/database.php';
+
+// get back sign up data if an error was encountered during signup 
+$firstname = $_SESSION['signup-data']['firstname'] ?? null;
+$lastname = $_SESSION['signup-data']['lastname'] ?? null;
+$username = $_SESSION['signup-data']['username'] ?? null;
+$email = $_SESSION['signup-data']['email'] ?? null;
+$createpassword = $_SESSION['signup-data']['createpassword'] ?? null;
+$confirmpassword = $_SESSION['signup-data']['confirmpassword'] ?? null;
+
+unset($_SESSION['signup-data']);
 ?>
 
 <!DOCTYPE html>
@@ -14,16 +25,22 @@ require_once 'config/database.php';
     <section class="form_section">
         <div class="container form_section-container">
             <h2>Sign Up</h2>
+            <?php
+            if(isset($_SESSION['signup'])): ?>
             <div class="alert_message error">
-                <p>This is an error message</p>
+                <p><?= $_SESSION['signup'];
+                    unset($_SESSION['signup']);
+                    ?>
+                </p>
             </div>
+            <?php endif ?>
             <form action="<?= ROOT_URL ?>signup-logic.php" enctype="multipart/form-data" method="post">
-                <input type="text" name="firstname" placeholder="First Name">
-                <input type="text" name="lastname" placeholder="Last Name">
-                <input type="text" name="username" placeholder="User Name">
-                <input type="email" name="email" placeholder="Email">
-                <input type="password" name="createpassword" placeholder="Create Password">
-                <input type="password" name="confirmpassword" placeholder="Confirm Password">
+                <input type="text" name="firstname" placeholder="First Name" value="<?=$firstname?>">
+                <input type="text" name="lastname" placeholder="Last Name" value="<?=$lastname?>">
+                <input type="text" name="username" placeholder="User Name" value="<?=$username?>">
+                <input type="email" name="email" placeholder="Email" value="<?=$email?>">
+                <input type="password" name="createpassword" placeholder="Create Password" value="<?=$createpassword?>">
+                <input type="password" name="confirmpassword" placeholder="Confirm Password" value="<?=$confirmpassword?>">
                 <div class="form_control">
                     <label for="avatar"></label>
                     <input type="file" name="avatar" id="avatar">
